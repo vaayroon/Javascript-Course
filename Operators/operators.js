7c75d1093cfb7488e0ccf006d2c142f76b8314de
B. Kevin Condor Romero (vaaroon) <vaaroon@outlook.com>
2023-11-05 12:22:15 +0100

    Add new HTML files and corresponding JavaScript files for Variables, Operators, Operators2, and Concatenation

---
+++ Operators/operators.js
@@ -0,0 +1,40 @@
+/* Asignation Operators:
+    =   x = y   x = y
+    +=  x += y  x = x + y
+    -=  x -= y  x = x - y
+    *=  x *= y  x = x * y
+    /=  x /= y  x = x / y
+    %=  x %= y  x = x % y
+    **= x **= y x = x ** y
+*/
+
+let number = 1;
+number += 1;
+document.write(number + "<br>");
+number -= 1;
+document.write(number + "<br>");
+number *= 5;
+document.write(number + "<br>");
+number /= 2;
+document.write(number + "<br>");
+number %= 2;
+document.write(number + "<br>");
+
+/* Numeric Operators:
+    +   Addition
+    -   Subtraction
+    *   Multiplication
+    /   Division
+    %   Modulus
+    **  Exponentiation
+    --  Decrement
+    ++  Increment
+*/
+
+let number1 = 1;
+let number2 = 2;
+
+let result = number1 + number2;
+document.write(result + "<br>");
+result++;
+document.write(result + "<br>");
---
+++ Variables/variables.js
@@ -0,0 +1,21 @@
+let string = "This is a string"
+let number = 1 //12,233,212121,548,4354,74,etc
+let boolean = true //false
+
+/* Keyword: var, let, and const 
+var --> global scope
+let --> block scope
+const --> block scope and cannot be reassigned
+*/
+
+/* Declare, Initialize and Assign variables:
+Declare --> var, let, const
+Initialize --> var name = value
+Assign --> name = value
+*/
+
+/* Special Data Types: undefined, null, NaN */
+console.log("🚀 ~ file: variables.js:2 ~ string:", string)
+let name = prompt("What is your name?")
+console.log("🚀 ~ file: variables.js:20 ~ name:", name)
+alert("Hello " + name)
