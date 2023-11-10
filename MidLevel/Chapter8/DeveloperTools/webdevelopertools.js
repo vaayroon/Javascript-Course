ee9d6d9af182ce7934c8b47d84e74e291929090c
Bryan Kevin Cóndor Romero <bryan.condor@dekra.com>
2023-11-10 20:14:42 +0100

    Add new HTML files and JavaScript files for MidLevel/Chapter7 and MidLevel/Chapter8

---
+++ MidLevel/Chapter8/DeveloperTools/webdevelopertools.js
@@ -0,0 +1,28 @@
+/* Web developer Tools:
+    * 1. Console
+    * 2. Debugger
+    * 3. Network
+    * 4. Performance
+    * 5. Memory
+    * 6. Application
+    * 7. Security
+    * 8. Audits
+    * 9. Lighthouse
+    * 16. Elements/Inspector
+    * 17. Sources
+*/
+
+/* Tell for what the tools are used for:
+    * 1. Console: To log the output of the code
+    * 2. Debugger: To debug the code
+    * 3. Network: To check the network status
+    * 4. Performance: To check the performance of the code
+    * 5. Memory: To check the memory usage of the code
+    * 6. Application: To check the application status
+    * 7. Security: To check the security of the code
+    * 8. Audits: To check the audits of the code
+    * 9. Lighthouse: To check the lighthouse of the code
+    * 16. Elements/Inspector: To check the elements of the code
+    * 17. Sources: To check the sources of the code
+
+*/
